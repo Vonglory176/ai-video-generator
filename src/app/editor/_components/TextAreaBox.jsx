c001e08353230214ac7ccb9c87481492f4f09e86
Vonglory176 <SkylerGConley@gmail.com>
2025-01-03 16:52:50 -0800

    Textarea additions/props

--- src/app/editor/_components/TextAreaBox.jsx
+++ src/app/editor/_components/TextAreaBox.jsx
@@ -1,11 +1,16 @@
 import { Textarea } from '@/components/ui/textarea'
 import React from 'react'
 
-const TextAreaBox = () => {
+const TextAreaBox = ({frame, handleInputChange}) => {
     return (
         <div className='flex flex-col gap-2'>
             <label className='text-sm font-medium'>Content</label>
-            <Textarea placeholder='Enter text here...' className='bg-white w-full h-full' />
+            <Textarea 
+                value={frame?.text}
+                onChange={(e) => handleInputChange(e.target.value)}
+                placeholder='Enter text here...' 
+                className='bg-white w-full h-full' 
+            />
         </div>
     )
 }
